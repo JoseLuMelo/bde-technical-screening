97da1ba3123a389f823c516303760f7dc33323fd
jose lucas melo <jose.lucasdemelo@gmail.com>
2025-05-25 14:53:26 -0300

    added negative coin values

--- 2-cash-minimizer.py
+++ 2-cash-minimizer.py
@@ -1,23 +1,32 @@
-def cash_minimizer(coins, money):
-    dp = [money + 1] * (money + 1)
-    dp[0] = 0
+def cash_minimizer(coins, amount):
+    from collections import deque
 
-    combination = [[] for _ in range(money + 1)]
+    queue = deque()
+    queue.append((0, 0, []))  # (current_sum, num_coins, coin_combination)
+    visited = set()
+    visited.add(0)
 
-    for i in range(1, money + 1):
-        for c in coins:
-            if i - c >= 0 and dp[i - c] + 1 < dp[i]:
-                dp[i] = dp[i - c] + 1
-                combination[i] = combination[i - c] + [c]
+    while queue:
+        current_sum, num_coins, coin_combination = queue.popleft()
 
-    if dp[money] != money + 1:
-        return dp[money], combination[money]
-    else:
-        return -1, []
+        if current_sum == amount:
+            return num_coins, coin_combination
+
+        for coin in coins:
+            next_sum = current_sum + coin
+            if next_sum not in visited:
+                visited.add(next_sum)
+                new_combination = coin_combination + [coin]
+                queue.append((next_sum, num_coins + 1, new_combination))
+
+    return -1, []
+
 
 coins = input()
 coins = list(map(float, coins.strip().split()))
 coins = [int(coin * 100) for coin in coins]
+neg_coins = [-1 * coin for coin in coins]
+coins = neg_coins + coins
 
 money = float(input())
 money = int(money * 100)
@@ -25,4 +34,4 @@ money = int(money * 100)
 n_coins, combination = cash_minimizer(coins, money)
 
 print("Number of coins required: ", n_coins)
-print("Coins combination: ", combination)
+print("combination: ", combination)
